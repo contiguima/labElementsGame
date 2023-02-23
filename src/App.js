fe973de1ea90e58d37d2352bf33ba7d13e4c0ad0
Constanza Guimaraez <77015241+contiguima@users.noreply.github.com>
2023-02-23 17:29:30 -0300

    Router

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
+import { Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import "./styles.css";
+
 export default function App() {
   return (
     <div className="App">
-      <Home />
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
     </div>
   );
 }
