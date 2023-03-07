050cb1a84714395e64ac69448bbd7a266d814f3b
Constanza Guimaraez <77015241+contiguima@users.noreply.github.com>
2023-03-06 21:28:38 -0300

    Clases, contexto y logica del darkMode

--- src/components/Topbar.jsx
+++ src/components/Topbar.jsx
@@ -1,19 +1,28 @@
 import { useNavigate } from "react-router-dom";
-import "../stylesheets/topbar.css";
+import "../stylesheets/topbar.scss";
 import moonLogo from "../assets/logos/moon.png";
 import arrowLogo from "../assets/logos/arrowBackBtn.png";
+import { ThemeContext } from "../context/themeContext";
+import { useContext } from "react";
 
 const Topbar = (props) => {
+  const { mode, toggleMode } = useContext(ThemeContext);
   const navigate = useNavigate();
 
   if (props.backTo != null) {
     return (
       <div className="topbar">
-        <button className="backBtn" onClick={() => navigate(props.backTo)}>
+        <button
+          className={mode === "light" ? "backBtnLight" : "backBtnDark"}
+          onClick={() => navigate(props.backTo)}
+        >
           {" "}
           <img src={arrowLogo} alt="arrow" />
         </button>
-        <button className="darkMode">
+        <button
+          className={mode === "light" ? "darkModeLight" : "darkModeDark"}
+          onClick={toggleMode}
+        >
           <img src={moonLogo} alt="moon,logo" />
         </button>
       </div>
@@ -21,9 +30,11 @@ const Topbar = (props) => {
   } else {
     return (
       <div className="topbar">
-        <button className="darkMode">
-          {" "}
-          <img src={moonLogo} alt="moon,logo" />{" "}
+        <button
+          className={mode === "light" ? "darkModeLight" : "darkModeDark"}
+          onClick={toggleMode}
+        >
+          <img src={moonLogo} alt="moon,logo" />
         </button>
       </div>
     );
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
+import { ThemeProvider } from "./context/themeContext";
 
 import App from "./App";
 
@@ -10,7 +11,9 @@ const root = createRoot(rootElement);
 root.render(
   <StrictMode>
     <BrowserRouter>
-      <App />
+      <ThemeProvider>
+        <App />
+      </ThemeProvider>
     </BrowserRouter>
   </StrictMode>
 );
